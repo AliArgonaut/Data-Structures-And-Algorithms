f2b7ec7ddb9831599efeabc28aae230dbdcffef7
lloyd aleister bonds <lloydabonds@gmail.com>
2025-12-02 19:03:47 -0500

    added JS implementation of two sum

---
+++ 2.JS_Two_Sum/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (y = 0; y < nums.length; y++){
+        const current = nums[y]
+        for (x = y+1; x < nums.length; x++){
+            if (current + nums[x] === target){
+                return [y, x]
+            }
+        }
+    }
+ };
